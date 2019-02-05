39e6c90a2360a796575183bf42b36a9542fe570b
Jesse Hall <jessehall3@gmail.com>
2019-02-05 13:47:24 -0500

    Now using ref and imperative input handling for the bill amount.

--- src/components/AddBill.js
+++ src/components/AddBill.js
@@ -1,30 +1,45 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import DatePicker from 'react-datepicker'
 import 'react-datepicker/dist/react-datepicker.css'
 
 function AddBill(props) {
-  const [amount, setAmount] = useState(0)
+  // const [amount, setAmount] = useState(0)
   const [category, setCategory] = useState(props.categories[0])
   const [date, setDate] = useState(new Date())
+
+  const inputBillAmount = useRef(0);
+  const onButtonClick = () => {
+    // `current` points to the mounted text input element
+    inputBillAmount.current.focus();
+  };
 
   const handleChangeDate = newDate => {
     setDate(newDate)
   }
 
-  const handleChangeAmount = e => {
-    const value = parseInt(e.target.value) || 0
-    setAmount(value, 10)
-  }
+  // const handleChangeAmount = e => {
+  //   const value = parseInt(e.target.value) || 0
+  //   setAmount(value, 10)
+  // }
 
   const handleChangeCategory = e => {
     setCategory(e.target.value)
   }
 
+  const isNum = function(rawNum) {
+    const num = parseInt(rawNum)
+    return typeof num == 'number' && !isNaN(num);
+  }
+
   const handleSubmit = e => {
     e.preventDefault()
-    if (!amount) {
+    const amount = inputBillAmount.current.value;
+    console.log("AMOUNT: ", amount);
+    if (!amount && amount !== 0) {
       alert('Please enter an amount')
       return
+    } else if(!isNum(amount)){
+      alert('Please enter a real number for the amount.')
     }
     props.onSubmit(amount, category || props.categories[0], date)
   }
@@ -35,7 +50,6 @@ function AddBill(props) {
       <div className="bg-white rounded shadow p-6 m-4 w-full lg:w-3/4 lg:max-w-lg">
         <div className="mb-4">
           <h1 className="text-grey-darkest">Enter a new Bill</h1>
-          <p>E.g. 'Electricity' or 'Gas' or 'Internet'</p>
           <div className="flex mt-4">
             <DatePicker selected={date} onChange={handleChangeDate} />
             <select onChange={handleChangeCategory}>
@@ -50,10 +64,9 @@ function AddBill(props) {
               : ''}
             </select>
             <input
-              className="shadow appearance-none border rounded w-full py-2 px-3 mr-4 text-grey-darker"
-              placeholder="Enter Bill"
-              value={amount}
-              onChange={handleChangeAmount}
+            className="shadow appearance-none border rounded w-full py-2 px-3 mr-4 text-grey-darker"
+            placeholder="Enter Bill Amount, eg $100.00"
+            ref={inputBillAmount}
             />
             <button
               className="flex-no-shrink p-2 border-2 rounded bg-teal text-white border-teal hover:text-white hover:bg-teal"
