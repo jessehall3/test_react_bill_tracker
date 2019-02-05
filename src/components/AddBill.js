1e553297129ace06ec7a127b9296c81aeaa68c09
Jesse Hall <jessehall3@gmail.com>
2019-02-05 10:00:51 -0500

    Showing more details in the Add Bill component.

--- src/components/AddBill.js
+++ src/components/AddBill.js
@@ -1,9 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-function AddBill() {
+function AddBill(props) {
+  const [amount, setAmount] = useState(0)
+
+  const handleChangeAmount = e => {
+    setAmount(parseInt(e.target.value), 10)
+  }
+
+  const handleSubmit = e => {
+    e.preventDefault()
+    if (!amount) {
+      alert('Please enter an amount.')
+      return
+    }
+    props.onSubmit(amount)
+  }
+
   return (
     <div className="App">
-      <h1>Add Bill</h1>
+      <form className="h-100 w-full flex items-center justify-center font-sans">
+      <div className="bg-white rounded shadow p-6 m-4 w-full lg:w-3/4 lg:max-w-lg">
+        <div className="mb-4">
+          <h1 className="text-grey-darkest">Enter a new Bill</h1>
+          <p>E.g. 'Electricity' or 'Gas' or 'Internet'</p>
+          <div className="flex mt-4">
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 mr-4 text-grey-darker"
+              placeholder="Enter Bill"
+              value={amount}
+              onChange={handleChangeAmount}
+            />
+            <button
+              className="flex-no-shrink p-2 border-2 rounded bg-teal text-white border-teal hover:text-white hover:bg-teal"
+              onClick={handleSubmit}
+            >
+              Add
+            </button>
+          </div>
+        </div>
+      </div>
+    </form>
     </div>
   )
 }
